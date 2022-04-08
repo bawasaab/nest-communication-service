b57233c418a8284167ff186bfc537d5685d4ebaa
DESKTOP-B421EOF\admin <deepak.bawa@orionesolutions.in>
2022-04-08 17:22:52 +0530

    receive event implemented

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './events/create-user.event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log(
+      'an user_created event received at communication-service',
+      data,
+    );
   }
 }
